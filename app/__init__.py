bee6d78b18546a64cdc33995561ac44afd05f4fc
Alex Piletski <don.tr.hacker@gmail.com>
2025-08-08 15:20:11 +0300

    ...

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
 
-def create_app():
-    app = Flask(__name__)
-    
-    # Ваши текущие настройки БД
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:pass@host/db'
-    
-    db.init_app(app)
-    
-    # Импорт моделей после инициализации db
-    from app import models
-    
-    return app
+from app import routes  # импортируем маршруты в самом конце
+
