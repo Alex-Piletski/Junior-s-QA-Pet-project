28652528defd5b2f5b7a53a8262caaf572b2587d
Alex Piletski <don.tr.hacker@gmail.com>
2025-08-08 11:44:25 +0300

    New port 5002

--- backend/app.py
+++ backend/app.py
@@ -7,5 +7,5 @@ def ping():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
 
