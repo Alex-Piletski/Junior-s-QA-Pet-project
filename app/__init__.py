102d9ad2106814dd6a3d7a29b113b8eb0f5f78ee
Alex Piletski <don.tr.hacker@gmail.com>
2025-08-08 14:33:06 +0300

    Add PostgreSQL connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,15 @@
+
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Импорт роутов ПОСЛЕ создания app
-from app import routes
+# Подключение к PostgreSQ
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    "postgresql://flask_db_postgres_user:yi2RNn9HoOREs1US9ZH5mjXBMrvnJCx2@"
+    "dpg-d2attlje5dus73c18f70-a:5432/flask_db_postgres"
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
