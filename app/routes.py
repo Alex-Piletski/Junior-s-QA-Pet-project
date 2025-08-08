bdd3aafaf96187c5f62f7dbfab7adc48b75633f7
Alex Piletski <don.tr.hacker@gmail.com>
2025-08-08 16:16:34 +0300

    ...

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,7 @@
 from flask import Blueprint, render_template
-from . import db
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def home():
     return render_template('index.html')
-
-@bp.route('/ping')
-def ping():
-    return {'status': 'ok'}
