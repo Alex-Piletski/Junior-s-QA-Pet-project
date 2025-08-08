48932328037353961bbe1e969a5000805c2d8dee
Alex Piletski <don.tr.hacker@gmail.com>
2025-08-08 14:05:53 +0300

    ...

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from app import app  # Теперь это безопасно
+from flask import render_template
 
-@app.route('/ping')
-def ping():
-    return {"status": "ok"}
+@app.route("/")
+def home():
+    return render_template("index.html")  # Отображаем ваш HTML-файл
